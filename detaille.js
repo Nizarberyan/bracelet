af9e025c73dc3dbdcf2f50c99ca838c73557828e
abdelkouddous <abdoalami.ru@gmail.com>
2025-01-27 14:59:45 +0100

    second

---
+++ detaille.js
@@ -0,0 +1,34 @@
+
+const menuIcon = document.querySelector('.menu-icon');
+const closeMenu = document.querySelector('.close-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const viewers = document.querySelectorAll('.viewers span');
+
+menuIcon.addEventListener('click', () => {
+    mobileMenu.classList.add('active');
+    document.body.style.overflow = 'hidden';
+});
+
+closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    document.body.style.overflow = 'auto';
+});
+
+function updateViewers() {
+    viewers.forEach(viewer => {
+        const currentCount = parseInt(viewer.textContent);
+        const randomChange = Math.floor(Math.random() * 10) - 5;
+        const newCount = Math.max(30, Math.min(60, currentCount + randomChange));
+        viewer.textContent = `${newCount} viewing now`;
+    });
+}
+
+setInterval(updateViewers, 15000);
+
+const quantityInput = document.querySelector('.quantity-input');
+
+quantityInput.addEventListener('input', function () {
+    this.classList.remove('quantity-changed');
+    void this.offsetWidth;
+    this.classList.add('quantity-changed');
+});
